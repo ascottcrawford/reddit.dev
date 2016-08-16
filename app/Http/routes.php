abf9d74ed954b49d80616e4081baa7be85722128
Scott <ascrawford@me.com>
2016-08-16 11:43:09 -0500

    Roll Dice Exercise

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,33 +11,57 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// Route::get('/sayhello/{name1}/{name2}', function ($name1, $name2) {
+// function view($relativePath, $data)
+
+// Route::get('/sayhello/{first_name}/{last_name?}', function ($foo, $name = 'World') {
 // 	return 'Hello Kings ' . $name1 . " and " . $name2;
 // });
 
-// Route::get('/sayhello/{name}', function($name)
+// Route::get('/sayhello/{first_name}/{last_name}', function($name)
 // {
 //     if ($name == "Chris") {
 //         return redirect('/');
 //     }
 
-//     return "Hello, $name!";
+//     $data = [
+//     	'firstName' => $foo,
+//     	'lastName' => $name,
+//     ];
+
+//     return view('my-first-view')->with('firstName', $foo); //single value
+//     // multiple values
+//     return view('my-first-view')->with($data);
+//     return view('my-first-view'), $data;
 // });
 
-Route::get('/uppercase/{name}', function($name)
+Route::get('/rolldice/{guessnumber}', function($guessnumber)
 {
-    if ($name == "Chris") {
-        return redirect('/');
-    }
-    $uppercase = strtoupper($name);
-    return $uppercase;
+    $dicenumber = rand(1, 6);
+
+    $data = [
+    	'dicenumber' => $dicenumber,
+    	'guessnumber' => $guessnumber
+    ];
+
+    // multiple values
+    return view('roll-dice')->with($data);
+  
 });
 
-Route::get('/add/{number}', function($number)
-{
-    return $number + 1;
-});
+// Route::get('/uppercase/{name}', function($name)
+// {
+//     if ($name == "Chris") {
+//         return redirect('/');
+//     }
+//     $uppercase = strtoupper($name);
+//     return $uppercase;
+// });
+
+// Route::get('/add/{number}', function($number)
+// {
+//     return $number + 1;
+// });
---
+++ resources/views/vendor/roll-dice.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Roll Dice</title>
+</head>
+<body>
+    <h1>Dice Guess <?= $dicenumber ?><?= $guessnumber ?></h1>
+</body>
+</html>
